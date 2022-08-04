51afb5ce59c5983a95dca5cf0e6dd4f92439d647
freddydc <freddydiaz.liv@gmail.com>
2022-08-04 18:14:45 -0500

    Add variables3.rs exercise

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,10 +1,13 @@
 mod variables1;
 mod variables2;
+mod variables3;
 
 use self::variables1::run_variables1;
 use self::variables2::run_variables2;
+use self::variables3::run_variables3;
 
 fn main() {
     run_variables1();
     run_variables2();
+    run_variables3();
 }
---
+++ variables/src/variables3.rs
@@ -0,0 +1,6 @@
+// variables3.rs
+
+pub fn run_variables3() {
+    let x: i32 = -20;
+    println!("Number {}", x);
+}
