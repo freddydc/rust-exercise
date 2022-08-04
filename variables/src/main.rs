16a240b13a3676abee3ca6366d6b73085fb1e391
freddydc <freddydiaz.liv@gmail.com>
2022-08-04 17:50:31 -0500

    Add variables2.rs exercise

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,10 @@
 mod variables1;
+mod variables2;
 
 use self::variables1::run_variables1;
+use self::variables2::run_variables2;
 
 fn main() {
     run_variables1();
+    run_variables2();
 }
---
+++ variables/src/variables2.rs
@@ -0,0 +1,10 @@
+// variables2.rs
+
+pub fn run_variables2() {
+    let x: u32 = 50;
+    if x == 10 {
+        println!("x is ten!");
+    } else {
+        println!("x is not ten!");
+    }
+}
