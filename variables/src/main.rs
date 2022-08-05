8d3faa84b843af2674ee3df99ea96eb113c1db73
freddydc <freddydiaz.liv@gmail.com>
2022-08-05 09:13:46 -0500

    Add variables4.rs exercise

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,13 +1,16 @@
 mod variables1;
 mod variables2;
 mod variables3;
+mod variables4;
 
 use self::variables1::run_variables1;
 use self::variables2::run_variables2;
 use self::variables3::run_variables3;
+use self::variables4::run_variables4;
 
 fn main() {
     run_variables1();
     run_variables2();
     run_variables3();
+    run_variables4();
 }
---
+++ variables/src/variables4.rs
@@ -0,0 +1,8 @@
+// variables4.rs
+
+pub fn run_variables4() {
+    let mut x = 300;
+    println!("Number {}", x);
+    x = 5000; // don't change this line
+    println!("Number {}", x);
+}
