fc861a4d28683f89b6860cfcf0ea0134d1dc2417
freddydc <freddydiaz.liv@gmail.com>
2022-08-05 09:29:25 -0500

    Add variables5.rs exercise

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -2,15 +2,18 @@ mod variables1;
 mod variables2;
 mod variables3;
 mod variables4;
+mod variables5;
 
 use self::variables1::run_variables1;
 use self::variables2::run_variables2;
 use self::variables3::run_variables3;
 use self::variables4::run_variables4;
+use self::variables5::run_variables5;
 
 fn main() {
     run_variables1();
     run_variables2();
     run_variables3();
     run_variables4();
+    run_variables5();
 }
---
+++ variables/src/variables5.rs
@@ -0,0 +1,8 @@
+// variables5.rs
+
+pub fn run_variables5() {
+    let number = "T-H-R-E-E"; // don't change this line
+    println!("Spell a Number : {}", number);
+    let number = 300; // don't rename this variable
+    println!("Number plus two is : {}", number + 2);
+}
