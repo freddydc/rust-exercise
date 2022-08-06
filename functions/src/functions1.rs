3eba533c97966c9540dc657989dddf2a574d9b8d
freddydc <freddydiaz.liv@gmail.com>
2022-08-06 18:34:22 -0500

    Add functions1.rs exercise

---
+++ functions/src/functions1.rs
@@ -0,0 +1,9 @@
+// functions1.rs
+
+fn call_me() {
+    println!("Hello!");
+}
+
+pub fn run_functions1() {
+    call_me();
+}
---
+++ functions/src/main.rs
@@ -0,0 +1,7 @@
+mod functions1;
+
+use functions1::run_functions1;
+
+fn main() {
+    run_functions1();
+}
