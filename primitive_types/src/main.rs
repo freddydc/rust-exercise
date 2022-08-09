b6a67820d559f06f2ae1cada789778c0f31b5be9
freddydc <freddydiaz.liv@gmail.com>
2022-08-09 10:20:49 -0500

    Add primitive_types3.rs exercise

--- primitive_types/src/main.rs
+++ primitive_types/src/main.rs
@@ -1,10 +1,13 @@
 mod primitive_types1;
 mod primitive_types2;
+mod primitive_types3;
 
 use primitive_types1::run_primitive_types1;
 use primitive_types2::run_primitive_types2;
+use primitive_types3::run_primitive_types3;
 
 fn main() {
     run_primitive_types1();
     run_primitive_types2();
+    run_primitive_types3();
 }
---
+++ primitive_types/src/primitive_types3.rs
@@ -0,0 +1,11 @@
+// primitive_types3.rs
+
+pub fn run_primitive_types3() {
+    let my_array = ['🍖'; 20];
+
+    if my_array.len() >= 10 {
+        println!("Wow, that's a big array!");
+    } else {
+        println!("Meh, I eat arrays like that for breakfast.");
+    }
+}
