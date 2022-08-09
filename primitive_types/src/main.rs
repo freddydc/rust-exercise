3a79e40d007d9e3870534bf467e08d17ba3a3867
freddydc <freddydiaz.liv@gmail.com>
2022-08-09 10:09:43 -0500

    Add primitive_types2.rs exercise

--- primitive_types/src/main.rs
+++ primitive_types/src/main.rs
@@ -1,7 +1,10 @@
 mod primitive_types1;
+mod primitive_types2;
 
 use primitive_types1::run_primitive_types1;
+use primitive_types2::run_primitive_types2;
 
 fn main() {
     run_primitive_types1();
+    run_primitive_types2();
 }
