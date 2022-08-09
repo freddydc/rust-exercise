1019d8ae55df6555d37eed2401a960100f2e8905
freddydc <freddydiaz.liv@gmail.com>
2022-08-09 09:56:03 -0500

    Add primitive_types1.rs exercise

---
+++ primitive_types/src/main.rs
@@ -0,0 +1,7 @@
+mod primitive_types1;
+
+use primitive_types1::run_primitive_types1;
+
+fn main() {
+    run_primitive_types1();
+}
---
+++ primitive_types/src/primitive_types1.rs
@@ -0,0 +1,15 @@
+// primitive_types1.rs
+
+pub fn run_primitive_types1() {
+    // Booleans (`bool`)
+
+    let is_morning = true;
+    if is_morning {
+        println!("Good morning!");
+    }
+
+    let is_evening = false;
+    if is_evening {
+        println!("Good evening!");
+    }
+}
