3a6649a7f8c3458866018027234d82ea901e5b03
freddydc <freddydiaz.liv@gmail.com>
2022-08-17 10:48:55 -0500

    Add structs2.rs exercise

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,7 +1,10 @@
 mod structs1;
+mod structs2;
 
 use structs1::run_structs1;
+use structs2::run_structs2;
 
 fn main() {
     run_structs1();
+    run_structs2();
 }
---
+++ structs/src/structs2.rs
@@ -0,0 +1,75 @@
+// structs2.rs
+
+#[derive(Debug)]
+struct Order {
+    name: String,
+    year: u32,
+    made_by_phone: bool,
+    made_by_mobile: bool,
+    made_by_email: bool,
+    item_number: u32,
+    count: u32,
+}
+
+pub fn run_structs2() {
+    let order_template = create_order_template();
+
+    let new_order = Order {
+        name: "Hacker in Rust".to_string(),
+        count: 4,
+        ..order_template
+    };
+
+    let Order {
+        name,
+        year,
+        made_by_phone,
+        made_by_mobile,
+        made_by_email,
+        item_number,
+        count,
+    } = new_order;
+
+    println!("Name: {}", name);
+    println!("Year: {}", year);
+    println!("Made by phone: {}", made_by_phone);
+    println!("Made by mobile: {}", made_by_mobile);
+    println!("Made by email: {}", made_by_email);
+    println!("Item number: {}", item_number);
+    println!("Count: {}", count);
+}
+
+fn create_order_template() -> Order {
+    Order {
+        name: String::from("Bob"),
+        year: 2019,
+        made_by_phone: false,
+        made_by_mobile: false,
+        made_by_email: true,
+        item_number: 123,
+        count: 0,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn your_order() {
+        let order_template = create_order_template();
+        let your_order = Order {
+            name: "Hacker in Rust".to_string(),
+            count: 1,
+            ..order_template
+        };
+
+        assert_eq!(your_order.name, "Hacker in Rust");
+        assert_eq!(your_order.year, order_template.year);
+        assert_eq!(your_order.made_by_phone, order_template.made_by_phone);
+        assert_eq!(your_order.made_by_mobile, order_template.made_by_mobile);
+        assert_eq!(your_order.made_by_email, order_template.made_by_email);
+        assert_eq!(your_order.item_number, order_template.item_number);
+        assert_eq!(your_order.count, 1);
+    }
+}
