58f45882f9a9d576db5b2c0b1e2606889712ed81
freddydc <freddydiaz.liv@gmail.com>
2022-08-18 10:35:58 -0500

    Add structs3.rs exercise

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,10 +1,13 @@
 mod structs1;
 mod structs2;
+mod structs3;
 
 use structs1::run_structs1;
 use structs2::run_structs2;
+use structs3::run_structs3;
 
 fn main() {
     run_structs1();
     run_structs2();
+    run_structs3();
 }
