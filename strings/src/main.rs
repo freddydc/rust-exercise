143edef20127f6342a0c17226a1f0c66298ac3ca
freddydc <freddydiaz.liv@gmail.com>
2022-08-22 10:20:57 -0500

    Add strings2.rs exercise

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,7 +1,10 @@
 mod strings1;
+mod strings2;
 
 use strings1::run_strings1;
+use strings2::run_strings2;
 
 fn main() {
     run_strings1();
+    run_strings2();
 }
---
+++ strings/src/strings2.rs
@@ -0,0 +1,15 @@
+// strings2.rs
+
+pub fn run_strings2() {
+    let word = String::from("green"); // Try not changing this line :)
+
+    if is_a_color_word(&word) {
+        println!("That is a color word I know!");
+    } else {
+        println!("That is not a color word I know.");
+    }
+}
+
+fn is_a_color_word(attempt: &str) -> bool {
+    attempt == "green" || attempt == "blue" || attempt == "red"
+}
