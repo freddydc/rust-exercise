32864ab8f226806e0a5e0b71cf90c0f59212cf38
freddydc <freddydiaz.liv@gmail.com>
2022-08-24 11:58:27 -0500

    Add strings4.rs exercise

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,13 +1,16 @@
 mod strings1;
 mod strings2;
 mod strings3;
+mod strings4;
 
 use strings1::run_strings1;
 use strings2::run_strings2;
 use strings3::run_strings3;
+use strings4::run_strings4;
 
 fn main() {
     run_strings1();
     run_strings2();
     run_strings3();
+    run_strings4();
 }
