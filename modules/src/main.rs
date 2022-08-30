80e032f82e25f21960834484856d36847dbf73cb
freddydc <freddydiaz.liv@gmail.com>
2022-08-30 08:57:23 -0500

    Add modules3.rs exercise

--- modules/src/main.rs
+++ modules/src/main.rs
@@ -1,10 +1,13 @@
 mod modules1;
 mod modules2;
+mod modules3;
 
 use self::modules1::run_modules1;
 use self::modules2::run_modules2;
+use self::modules3::run_modules3;
 
 fn main() {
     run_modules1();
     run_modules2();
+    run_modules3();
 }
---
+++ modules/src/modules3.rs
@@ -0,0 +1,12 @@
+// modules3.rs
+// You can use the 'use' keyword to bring module paths from modules from anywhere
+// and especially from the Rust standard library into your scope.
+
+use std::time::{SystemTime, UNIX_EPOCH};
+
+pub fn run_modules3() {
+    match SystemTime::now().duration_since(UNIX_EPOCH) {
+        Ok(n) => println!("1970-01-01 00:00:00 UTC was {} seconds ago!", n.as_secs()),
+        Err(_) => panic!("SystemTime before UNIX EPOCH!"),
+    }
+}
