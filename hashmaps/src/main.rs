f166e16fc563553c245c83cd7acb4add049b334e
freddydc <freddydiaz.liv@gmail.com>
2022-09-05 10:27:11 -0500

    Add hashmaps3.rs exercise

--- hashmaps/src/main.rs
+++ hashmaps/src/main.rs
@@ -1,10 +1,13 @@
 mod hashmaps1;
 mod hashmaps2;
+mod hashmaps3;
 
 use self::hashmaps1::run_hashmaps1;
 use self::hashmaps2::run_hashmaps2;
+use self::hashmaps3::run_hashmaps3;
 
 fn main() {
     run_hashmaps1();
     run_hashmaps2();
+    run_hashmaps3();
 }
