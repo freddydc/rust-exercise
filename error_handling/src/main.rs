166f66d723d62c1d62af3de3b44518594242411c
freddydc <freddydiaz.liv@gmail.com>
2022-09-19 10:43:28 -0500

    feat: add errors5.rs exercise

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -2,15 +2,21 @@ mod errors1;
 mod errors2;
 mod errors3;
 mod errors4;
+mod errors5;
 
 use self::errors1::run_errors1;
 use self::errors2::run_errors2;
 use self::errors3::run_errors3;
 use self::errors4::run_errors4;
+use self::errors5::run_errors5;
 
 fn main() {
     run_errors1();
     run_errors2();
     run_errors3().unwrap();
     run_errors4();
+
+    if let Err(e) = run_errors5() {
+        println!("{}", e);
+    }
 }
